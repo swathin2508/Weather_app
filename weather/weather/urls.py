9d2a2b3e80b5cc0302534607b673494db461deff
swathin2508 <165947245+swathin2508@users.noreply.github.com>
2024-04-24 14:23:51 +0530

    Update urls.py

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+'''urls of weather project directory'''
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('main.urls')), 
